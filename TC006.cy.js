fd69cfd3526c7c8df0c5066ffc71bebe2dac3a3f
praveenajangam <92263135+praveenajangam@users.noreply.github.com>
2022-11-29 21:18:12 +0530

    Add files via upload

---
+++ TC006.cy.js
@@ -0,0 +1,48 @@
+//const { expect } = require("chai")
+
+describe('TC006', () => {
+    const productName = 'Prod-46'
+    const projectName = 'project333'
+  
+    it("Login to the Design Studio", () => {
+        cy.visit('https://gdfstudio.qa.concora.com', { failOnStatusCode: false })
+        cy.wait(2000)
+        cy.get('button').contains('Log In').click()
+        //cy.get('input[type=email]').type('praveena.jangam@concora.com') 
+        //cy.get('input[placeholder="Enter your email address"]').type('praveena.jangam@concora.com') 
+        cy.get('.sc-ejRpRk > .exUtJa > .sc-ezWOiH').type('praveena.jangam@concora.com'); 
+        cy.get('.sc-ejRpRk > .jHDfNU > .sc-ezWOiH').type('RohithCharan25#');
+        cy.get('.sc-ejRpRk > .sc-bZkfAO').click()
+        cy.wait(5000)
+        cy.url().should('eq', 'https://gdfstudio.qa.concora.com/')
+    })
+   
+    it("Create and downaload Brochure", () => {
+        cy.get('div').contains('Create a Brochure').click()
+        cy.wait(2000)
+        
+        cy.get('#useExistProject').click()
+        //cy.get('#startNewProject').click()
+        //following code is for existing project
+        //cy.get('.css-qc6sy-singleValue').select(projectName,{force: true} )
+        cy.get('#react-select-2-input').type(projectName)
+        //cy.get('select').select(projectName)
+        cy.get('button').contains('Select Project').click();
+        //double clicking to move the focus
+        cy.get('button').contains('Select Project').click()
+        cy.wait(2000)
+
+        cy.get('.sc-wAnfg > .sc-bczRLJ').click()
+        cy.wait(2000)
+        cy.get('#btn-edit-project-details').click()
+        cy.wait(2000)
+        cy.get(':nth-child(1) > .sc-fmGnzW > :nth-child(1)').click()
+        cy.wait(2000)
+        cy.get('#btn-edit-project-details').click()
+        cy.wait(2000)
+        cy.get('#btn-review-submittal').click()
+        cy.wait(2000)
+        cy.get('#btn-download-submittal').click()
+    })
+  
+})
